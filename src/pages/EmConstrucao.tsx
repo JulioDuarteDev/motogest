a771b7dfc0820de2a07b17092d532883b7256914
Julio Duarte <julio.franca.017@ufrn.edu.br>
2024-08-04 16:58:42 -0300

    #31 feat: altera lógica do componente "em construção"

--- src/pages/EmConstrucao.tsx
+++ src/pages/EmConstrucao.tsx
@@ -1,16 +1,15 @@
-import { Link } from "react-router-dom";
-import { buttonVariants } from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router-dom";
 
 export function EmConstrucao() {
+	const navigate = useNavigate();
 	return (
 		<div className="flex flex-col grow items-center justify-center p-4 gap-6">
 			<h1>Em construção</h1>
 			<p className="text-md text-primary">
 				A página que você está tentando acessar ainda não foi concluída.
 			</p>
-			<Link className={buttonVariants({ variant: "default" })} to="/">
-				Voltar para o início
-			</Link>
+			<Button role="link" onClick={() => navigate(-1)}>Voltar</Button>
 		</div>
 	);
 }
