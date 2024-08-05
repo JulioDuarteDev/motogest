8e8ac4ebb9bc80381200c8871823d9fa3b9adca5
Julio Duarte <julio.franca.017@ufrn.edu.br>
2024-08-05 02:51:09 -0300

    #37 feat: ajusta layout motos disponíveis

--- src/components/CardMotoDisponivel.tsx
+++ src/components/CardMotoDisponivel.tsx
@@ -1,5 +1,5 @@
 import FallbackImage from "@/assets/img/fallback_moto.jpg";
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
 	Dialog,
 	DialogContent,
@@ -10,66 +10,77 @@ import {
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button.tsx";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from "@/components/ui/carousel";
 
-interface CardMotoDisponivelProps {
-	ano: number;
-	cor?: string;
-	cilindrada: number;
-	quilometragem: number;
-	marca: string;
-	modelo: string;
-	url_imagem?: string;
-}
-
-export function CardMotoDisponivel({
-	modelo,
-	ano,
-	cor,
-	marca,
-	quilometragem,
-	url_imagem,
-	cilindrada
-}: CardMotoDisponivelProps) {
+export function CardMotoDisponivel({ moto }) {
 	return (
-		<div>
-			<Dialog>
-				<DialogTrigger>
-					<Card role={"listitem"} className="w-[300px]">
-						<CardHeader className="text-center">
-							<img
-								alt={"Imagem de " + modelo}
-								src={url_imagem || FallbackImage}
-							></img>
-							<CardTitle className="text-base">
-								{modelo} ({ano})
-							</CardTitle>
-						</CardHeader>
-					</Card>
-				</DialogTrigger>
-				<DialogContent
-					aria-describedby={undefined}
-					className="sm:max-w-[425px]"
-				>
-					<DialogHeader>
+		<Dialog>
+			<DialogTrigger>
+				<Card role="listitem" className="h-[250px]">
+					<CardHeader className="text-center">
 						<img
-							alt={"Imagem de " + modelo}
-							src={url_imagem || FallbackImage}
+							className="w-full h-40"
+							alt={"Imagem de " + moto.nome}
+							src={moto.variacoes[0].url || FallbackImage}
 						></img>
-						<DialogTitle className="text-center">{modelo}</DialogTitle>
-					</DialogHeader>
-					<ul>
-						<li><span className="font-bold">Ano: </span>{ano}</li>
-						<li><span className="font-bold">Marca:  </span>{marca}</li>
-						<li><span className="font-bold">Cilindrada: </span>{cilindrada}</li>
-						<li><span className="font-bold">Cor: </span>{cor}</li>
-						<li><span className="font-bold">Quilometragem: </span>{quilometragem}</li>
-					</ul>
-					<DialogFooter className="sm:justify-center">
-						<Button className="w-full"> <FontAwesomeIcon size="xl" icon={faWhatsapp} className="pr-2"/> Entrar em contato</Button>
-					</DialogFooter>
-				</DialogContent>
-			</Dialog>
-		</div>
+						<CardTitle className="text-base">{moto.nome}</CardTitle>
+					</CardHeader>
+				</Card>
+			</DialogTrigger>
+			<DialogContent aria-describedby={undefined} className="max-w-[500px]">
+				<DialogHeader className="container">
+					{moto.variacoes.length > 1 ? (
+						<Carousel className="w-full p-2">
+							<CarouselContent>
+								{moto.variacoes.map((cor, index) => (
+									<CarouselItem key={index}>
+										<Card>
+											<CardContent>
+												<img className="h-auto" src={cor.url} alt={cor.cor} />
+											</CardContent>
+										</Card>
+									</CarouselItem>
+								))}
+							</CarouselContent>
+							<CarouselPrevious />
+							<CarouselNext />
+						</Carousel>
+					) : (
+						<img
+							alt={"Imagem de " + moto.modelo}
+							src={moto.variacoes[0].url || FallbackImage}
+						></img>
+					)}
+					<DialogTitle className="text-center">{moto.nome}</DialogTitle>
+				</DialogHeader>
+				<dl className="space-y-3">
+					<div className="flex gap-2">
+						<dt className="font-semibold">Marca:</dt>
+						<dd>{moto.marca}</dd>
+					</div>
+					<div className="flex gap-2">
+						<dt className="font-semibold">Cilindradas:</dt>
+						<dd>{moto.cilindrada}cc</dd>
+					</div>
+					<div className="flex gap-2">
+						<dt className="font-semibold">Quantidade disponível:</dt>
+						<dd>{moto.quantidade}</dd>
+					</div>
+				</dl>
+				<DialogFooter className="justify-center">
+					<Button className="w-full">
+						<FontAwesomeIcon size="xl" icon={faWhatsapp} className="pr-2" />{" "}
+						Entrar em contato
+					</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
 	);
 }
